f9a6bec02482828cab9c62cdbc39dbea128cfa49
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-01-13 14:53:59 +0100

    lade till api:et

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let url = "adressen";
+let url = "https://newsapi.org/v2/everything?q=keyword&apiKey=d83b8fc981ee4157944ca434e8a4c295";
 fetch(url)
   .then((response) => response.json())
   .then((data) => {

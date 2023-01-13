88074acf4905b96c9719b3b033648d0b7349864b
Danyeal <95497503+danyealmateen@users.noreply.github.com>
2023-01-13 14:59:49 +0100

    uppdaterat script med button & eventlistener

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let url = "https://newsapi.org/v2/everything?q=keyword&apiKey=d83b8fc981ee4157944ca434e8a4c295";
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+let buttonForRenderingNewspaper = document.getElementById(
+  "buttonForRenderingNewspaper"
+);
+
+buttonForRenderingNewspaper.addEventListener("click", () => {
+  renderNewsPaper();
+  console.log("hej du har klickat mig");
+});
+
+let url =
+  "https://newsapi.org/v2/everything?q=keyword&apiKey=d83b8fc981ee4157944ca434e8a4c295";
+
+function renderNewsPaper() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}

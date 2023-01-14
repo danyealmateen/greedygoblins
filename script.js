386a0308cfe700d5ed5a372bdc9a7782e7fb34a6
Danyeal <95497503+danyealmateen@users.noreply.github.com>
2023-01-14 12:21:57 +0100

    added bg img, api rendering in divs, and styling

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-//The button that fires the function renderNewsPaper()
-let buttonForRenderingNewspaper = document.getElementById(
-  "buttonForRenderingNewspaper"
-);
-
-
-let articleOne = document.getElementById("article-one");
-
-buttonForRenderingNewspaper.addEventListener("click", () => {
+let renderArticles = document.getElementById("renderArticles");
+renderArticles.addEventListener("click", () => {
   renderNewsPaper();
   console.log("hej du har klickat mig");
 });
+
+let articleDiv = document.createElement("div");
+articleDiv.setAttribute("id", "articleDiv");
 
 let url =
   "https://newsapi.org/v2/everything?q=keyword&apiKey=d83b8fc981ee4157944ca434e8a4c295";
@@ -19,6 +15,19 @@ function renderNewsPaper() {
     .then((response) => response.json())
     .then((data) => {
       data.articles.forEach((obj) => {
+        articleDiv.innerHTML += `
+        <h2>Title:</h2> ${obj.title}
+        </br>
+        <h2>Author:</h2> ${obj.author}
+        </br>
+        <h2>Content:</h2> ${obj.content}
+        </br>
+        <h2><a href="${obj.url}">Click here for the full article</a></h2>
+        </br>
+        <hr>
+        </br>
+        `;
+        document.body.appendChild(articleDiv);
         console.log(obj);
       });
 
